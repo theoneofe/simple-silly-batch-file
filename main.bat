e9138444db5bde4b7cf61af1a0eca4baa0ff2e29
Amber Crimsonwood <132849265+theoneofe@users.noreply.github.com>
2023-07-02 21:15:49 +0100

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,43 @@
+@echo off
+
+rem This script is a simple batch file.
+rem This file WILL NOT cause harm to your system.
+
+rem If you want to learn about these fake prank "hacking" scripts, visit <https://www.tumblr.com/theoneofe/721764990007361536/fake-prank-scripts?source=share>
+rem This script was authored by Amber Crimsonwood (theoneofe).
+
+rem DO NOT USE THIS FOR ILLICIT ACTIVITES, because that's f*cking illegal.
+
+echo Hi! Welcome to the hacking script of all time.
+echo Where would you like to go today?
+
+echo 1. Zeus Wirus
+echo 2. Install Windows Vista
+echo 3. Install Kali Linux
+echo 6. Install DLC (Options 4 and 5)
+
+set /p num=Enter your choice: 
+
+if "%num%"=="1" (
+	echo Loading Zeus Wirus...
+	ping localhost -n 5 >nul
+    echo Zeus Wirus could not connect to the Internet.
+) else if "%num%"=="2" (
+    echo No product key found. This is illegal you know.
+) else if "%num%"=="3" (
+	echo Downloading Kali Linux ISO...
+	ping localhost -n 2 >nul
+    echo Could not access the Kali Linux servers.
+) else if "%num%"=="4" (
+    echo Choice 4 unavailable. Please Install DLC.
+) else if "%num%"=="5" (
+    echo Choice 5 unavailable. Please Install DLC.
+) else if "%num%"=="6" (
+	echo Loading payment options...
+	ping localhost -n 5 >nul
+    echo Could not connect to the Pressurised Gas servers.
+) else (
+    echo Invalid input. Please enter a number from 1 to 6.
+)
+
+pause
